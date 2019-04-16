6ea7ff56d421f2a151065fee7be853bc2579b273
nrhint <nathan@hintonclan.org>
2019-04-15 20:48:46 -0600

    working on a clone of FlipOut

---
+++ FlipOutV2/FlipOutV1-1.py
@@ -0,0 +1,14 @@
+##Nathan Hinton
+#This is a clone of flip out.
+##Goals:
+# 1) Larger game board
+
+import pygame
+
+class Chip:
+    def __init__(self, posx, posy, color):
+        pass
+    def drop(self):
+        pass
+    def score(self):
+        pass
