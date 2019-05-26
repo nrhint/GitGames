d7b7cbd58dc228b2be0bb87b659aa2ae39d733e7
nrhint <nathan@hintonclan.org>
2019-05-25 18:36:51 -0600

    learning and working.

--- EmpireBuilder/Main.py
+++ EmpireBuilder/Main.py
@@ -1,1 +1,38 @@
 ##Nathan Hinton
+
+#Import:
+import pygame
+from constants import *
+
+class Interface():#This will be for multiplayer later
+    def __init__(self):
+        print("Interface Init")
+
+class Player():#This is for the player and will handle all of the input from the user to the Screen class
+    def __init__(self, color, name):
+        print("Player Init")
+
+class Screen():#This class is for all of the drawing
+    def __init__(self, width = 300, height = 200):
+        print("Screen Init")
+        #width, height = 300, 200
+        screen = pygame.display.set_mode((width, height))
+        screen.fill(WHITE)
+        pygame.display.flip()
+        self.width = width
+        self.height = height
+    def update(self):
+        pass
+
+def main():
+    print("Starting Main...")
+    screen = Screen()
+    running = True
+    while running:
+        screen.update()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+                pygame.quit()
+
+if __name__ == '__main__':main()
---
+++ EmpireBuilder/constants.py
@@ -0,0 +1,8 @@
+##Nathan Hinton
+##This will have all of the constant valuse used in this game.
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
