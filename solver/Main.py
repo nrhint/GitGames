697804825e70363b08987f92ff157e96a97fd541
nrhint <nathan@hintonclan.org>
2019-05-29 09:33:12 -0600

    working on rebuilding the suduku solver

---
+++ solver/Main.py
@@ -0,0 +1,18 @@
+##Nathan Hinton
+##
+
+def loadData(file):
+    pass
+
+def getRow(data):
+    pass
+def getCol(data):
+    pass
+def getBox(data):
+    pass
+def checkForMatch(data):
+    pass
+def main():
+    print("Main")
+
+if __name__ == '__main__':main()
