6d87b3396b2ec48a2c49f65678ce9b6dc720aefe
nrhint <nathan@hintonclan.org>
2019-05-30 17:27:26 -0600

    still problem with the box function

--- solver/Main.py
+++ solver/Main.py
@@ -1,20 +1,6 @@
 ##Nathan Hinton
 
 ##TO DO:
-"""
-Main
-8 1
-[]
-[1, 10, 19, 28, 37, 46, 55, 64, 73]
-should be: [0, 9, 18, ..., 72]
-ROW:
-[]
-COL:
-['7', '5', '3', '1', '9', '8']
-
-['7', '5', '3', '1', '9', '8']
-['2', '4', '6']
-"""
 
 file = 'File1.txt'
 numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
@@ -30,7 +16,6 @@ def processData(rawData):#I am going to store the numbers in a map so that I can
     data = {}
     x = 0
     for number in rawData:
-        #print(x)
         if number in numbers:
             data.update({x:number})
             x +=1
@@ -41,13 +26,8 @@ def processData(rawData):#I am going to store the numbers in a map so that I can
             x +=1
     return data
 def getRow(data, rowNum):
-    
-    #rowNum = (rowNum-1)*9
     start = (rowNum-1) *9
-    print(start)
-    
     end = (rowNum-1) *9+8
-    print(end)
     row = []
     for item in data:
         if int(item)>=start and int(item)<=end:
@@ -60,36 +40,47 @@ def getCol(data, colNum):
             col.append(item)
             #print(item)
     return col
-def getBox(data):
-    pass
+def getBox(data, col, row):
+    nums = []
+    baseCol = int((col-1)/3)*3
+    colRange = [baseCol, baseCol+1, baseCol+2]
+    baseRow = int((row-1)/3)*3
+    rowRange = [baseRow, baseRow+1, baseRow+2]
+    for cols in colRange:
+        for rows in rowRange:
+            nums.append(getNumber(cols, rows))
+    return nums
+def getNumber(col, row):
+    place = ((row-1)*9)-1+col
+    return place
 def findColRow(number):
     col = (number%9)+1
     row = int(number/9)+1
-    return row, col
+    return col, row
 def checkForMatch(data, number):
     options = []
     if data[number] in numbers:
         return [number]
-    row, col = findColRow(number)
-    #print(row, col)
-    rowNums = getRow(data, row)
+    col, row = findColRow(number)
+    rowNums = getRow(data, row)#Cross hatch the place:
     colNums = getCol(data, col)
-    print(rowNums)
-    print(colNums)
-    print()
-    print("ROW:")
-    print(getValues(data, rowNums))
-    print("COL:")
-    print(getValues(data, colNums))
-    nums = getValues(data, rowNums+colNums)
-    print()
-    print(nums)
+    box = getBox(data, col, row)#Get the numbers in the box
+    nums = getValues(data, rowNums+colNums+box)
     for testValue in numbers:
         if testValue in nums:
             pass
         else:
             options.append(testValue)
-    return options
+    print(options)
+    if len(options) == 1:
+        return int(options[0])
+    else:
+        return 0
+def method1(data):
+    newData = data
+    for place in data:
+        newData = solveSpace(newData, place)
+    return newData
 def getValues(data, nums):
     dataOut = []
     try:
@@ -107,13 +98,8 @@ def getValues(data, nums):
             return -1
 def solveSpace(data, number):
     result = checkForMatch(data, number)
-    if len(result) == 1:
-        print("Number found!")
-        data.update({number:result})
-    else:
-        pass
-def findUnsolvedSpace(data):
-    pass
+    data.update({number:result})
+    return data
 def main():
     print("Main")
     #file = input("Filename: ")
