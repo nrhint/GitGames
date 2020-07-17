8341372bd00d94b9cc961819c389dbfbf26e445c
nrhint <nathan@hintonclan.org>
2020-07-17 13:18:37 -0600

    added a scaler var to the bullets in astro

--- Astroids/bullet.py
+++ Astroids/bullet.py
@@ -19,11 +19,13 @@ class Bullet:
         self.y = y
         self.heading = heading
         self.img = pygame.image.load('bullet.png')
+        bulletScaleFactor = 10
+        self.img = pygame.transform.scale(self.img, (self.img.get_width()*bulletScaleFactor, self.img.get_height()*bulletScaleFactor))
         self.speed = speed
         self.dist = 0
         self.delete = False
-        self.size = 2
-        self.rect = pygame.Rect((self.x+9, self.y+9), (2, 2))
+        self.size = self.img.get_width()
+        self.rect = pygame.Rect((self.x+(self.img.get_width()-1), self.y+(self.img.get_height()-1)), (self.size, self.size))
     def run(self):
         if self.dist > distance:
             self.delete = True
@@ -32,6 +34,6 @@ class Bullet:
             self.y+= -(cos(radians(self.heading)))* self.speed
             self.x+= -(sin(radians(self.heading)))* self.speed
     def update(self):
-        self.rect = pygame.Rect((self.x%self.display.get_width()+9, self.y%self.display.get_height()+9), (self.size, self.size))
+        self.rect = pygame.Rect((self.x+(self.img.get_width()-1), self.y+(self.img.get_height()-1)), (self.size, self.size))
         self.display.blit(self.img, (self.x%self.display.get_width(), self.y%self.display.get_height()))
-#        pygame.draw.rect(self.display, (0, 0, 255), self.rect)
+        pygame.draw.rect(self.display, (0, 0, 255), self.rect)
